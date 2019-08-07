f9a647eaa39c679c9af76b31d46d85dc86530b62
Hugo <hugovk@users.noreply.github.com>
2019-08-07 17:10:41 +0300

    Drop support for EOL Python 3.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     "Only Push API (Team Inbox and Chat) and Streaming API available at this moment.",
     license="LGPLv3",
     keywords="flowdock api wrapper",
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
@@ -27,7 +27,6 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
